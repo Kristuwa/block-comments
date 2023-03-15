8db0642ee0393e0db8dae86da25593e72da3310e
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2023-03-15 20:11:15 +0300

    add all tsks

--- src/js/form.js
+++ src/js/form.js
@@ -1,9 +1,11 @@
 let form = document.querySelector('.callback-form');
 let listComments = document.querySelector('.section-cases__list');
+let inputDateForm = document.querySelector('#start');
 
 form.addEventListener('submit', onFormSubmit);
 listComments.addEventListener('click', onCloseBtn);
 listComments.addEventListener('click', onLikeBtn);
+inputDateForm.addEventListener('input', onInputDate);
 
 let formData = {};
 
@@ -16,9 +18,15 @@ function onFormSubmit(e) {
     formData[name] = value;
   });
 
+  changeDateForComment();
+
   addToHTML(formData);
 
   form.reset();
+}
+
+function addToHTML(data) {
+  return listComments.insertAdjacentHTML('beforeend', markupComment(data));
 }
 
 function markupComment({ name, comment, date }) {
@@ -27,16 +35,12 @@ function markupComment({ name, comment, date }) {
  
   <div class="section-cases__text-content"><p class="section-cases__comment">${comment}</p></div>
   <p class="section-cases__date">${date}</p>
-  <button type="button" class='section-cases__btn-close btn' aria-label="close"><img src="./images/bin.svg" width="32" height="32" alt="bin"></button>
-<button type="button" class='section-cases__btn-like btn' aria-label="like"><span class="count">0</span><svg
+  <button type="button" class='section-cases__btn-close btn' aria-label="close"></button>
+<button type="button" class='section-cases__btn-like btn' aria-label="like"><span class="count">0</span><span><svg
 width="32" height="32" class="like-btn">
-<use xlink:href="./images/icons.svg#icon-heart"></use>
-</svg></button>
+<use href="./images/icons.svg#icon-heart"></use>
+</svg></span></button>
 </li>`;
-}
-
-function addToHTML(data) {
-  return listComments.insertAdjacentHTML('beforeend', markupComment(data));
 }
 
 function onCloseBtn(e) {
@@ -65,5 +69,70 @@ function onLikeBtn(e) {
       count -= 1;
     }
     counter.innerHTML = count;
+  }
+}
+
+function onInputDate(e) {
+  year = Number(e.target.value.split('-')[0]);
+  month = Number(e.target.value.split('-')[1]);
+  day = Number(e.target.value.split('-')[2]);
+
+  const date = new Date(year, month - 1, day);
+  const dateInMs = date.getTime();
+  const nowDate = Date.now();
+
+  if (dateInMs > nowDate) {
+    e.target.value = '';
+    alert('Выберете текущую дату, либо ранее!!');
+    return;
+  }
+}
+
+function changeDateForComment() {
+  let year = Number(formData.date.split('-')[0]);
+  let month = Number(formData.date.split('-')[1]);
+  let day = Number(formData.date.split('-')[2]);
+  let dateNow = new Date();
+  let hours = dateNow.getHours();
+  let minutes = dateNow.getMinutes();
+
+  const date = new Date(year, month - 1, day);
+  const dateInMs = date.getTime();
+
+  let hours24ToMs = -86400000;
+  let yesterdayTime =
+    -hours * 60 * 60 * 1000 -
+    minutes * 60 * 1000 -
+    dateNow.getSeconds() * 1000 -
+    dateNow.getMilliseconds() +
+    hours24ToMs;
+
+  const result = dateInMs - dateNow.getTime();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  if (day < 10) {
+    day = `0${day}`;
+  }
+
+  if (
+    (Number(result) >= hours24ToMs && Number(result) < 0) ||
+    formData.date === ''
+  ) {
+    formData.date = `сегодня, ${hours}:${minutes}`;
+  } else if (Number(result) === yesterdayTime) {
+    formData.date = `вчера, ${hours}:${minutes}`;
+  } else {
+    formData.date = `${day}.${month}.${year}, ${hours}:${minutes}`;
   }
 }
